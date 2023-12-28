0a6f0e41e5cfe0270517c83f00668ede471cac04
Yawar <rehaanyawer57@gmail.com>
2023-12-28 02:35:26 -0800

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './app.scss';
+import Navbar from './components/Navbar/Navbar';
+const App = () => {
+  return (
+    <div>
+      <section id='Homepage'>
+        <a href=''>
+          <Navbar />
+        </a>
+      </section>
+      <section id='Services'>Parallax</section>
+      <section>Services</section>
+      <section id='Portfolio'>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id='Contact'>Contact</section>
+      {/* <Test /> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import './navbar.scss';
+import { motion } from 'framer-motion';
+import Sidebar from '../Sidebar/Sidebar';
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      {/* Sidebar */}
+      <Sidebar />
+      <div className='wrapper'>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Yawar Mushtaq
+        </motion.span>
+        <div className='social'>
+          <a href='#'>
+            <img src='/facebook.png' alt='' />
+          </a>
+          <a href='#'>
+            <img src='/instagram.png' alt='' />
+          </a>
+          <a href='#'>
+            <img src='/youtube.png' alt='' />
+          </a>
+          <a href='#'>
+            <img src='/dribbble.png' alt='' />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
